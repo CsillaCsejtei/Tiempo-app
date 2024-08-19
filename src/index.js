1f63ba7212c321e01a5442ddb7dd48b07f0168aa
CsillaCsejtei <csillacsejtei@gmail.com>
2024-08-19 20:23:12 +0100

    Added the search funcionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSearchCity);
+
 function handleSearchCity(event) {
   event.preventDefault();
   let search = document.querySelector("#search-form-input");
-  let city = document.querySelector("#city");
-  city.innerHTML = search.value;
+  searchCity(search.value);
 }
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", handleSearchCity);
+
+function searchCity(city) {
+  let apiKey = "a7f4435600oc7ca6bdf3abed988ftf39";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+function updateWeather(response) {
+  let temperature = document.querySelector("#temperature");
+  let originCity = document.querySelector("#city");
+  originCity.innerHTML = respnse.data.city;
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+}
